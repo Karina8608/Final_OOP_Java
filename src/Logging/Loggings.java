d9bfbe048ec5b31c0ebed6bf67e335093ccec554
Karina <>
2024-02-18 12:18:58 +0300

    Финальная работа по ООР

---
+++ src/Logging/Loggings.java
@@ -0,0 +1,21 @@
+package Logging;
+
+public interface Loggings {
+
+    /**
+     * Запись выражения в файл
+     * @param expression - математическое выражение в виде отформатированной строки
+     * @param nameFile - путь к файлу
+     */
+    public void writeFile(String expression, String nameFile);
+
+    /**
+     * Метод получения математического выражения в виде отформатированной строки
+     * @param x - первое число
+     * @param y - второе число
+     * @param result - резeльтат
+     * @param operation - математическое действие
+     * @return - отформатированная строка
+     */
+    public String getExpression(Double x, Double y, Double result, Integer operation);
+}
---
+++ src/Operations/Model1.java
@@ -0,0 +1,13 @@
+package Operations;
+
+/**
+ * Model1
+ */
+public interface Model1 {
+
+    public Double result();
+
+    public void setX(Double value);
+
+    public void setY(Double value);
+}
